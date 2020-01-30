87800da5e3286a79a48bbe80c1df058fd67c567f
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-30 10:08:56 -0600

    adding style at the form with materialize

--- src/component/Formulario.js
+++ src/component/Formulario.js
@@ -1,10 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../index.css';
 
 const Formulario = () => {
+
+  // State del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [error, setError] = useState(false);
+
+  // Extraer ciudad y país
+  const { ciudad, pais } = busqueda;
+
+  // Función que coloca los elementos en el state
+  const handleChange = e => {
+    // Actualizar el state
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name] : e.target.value
+    });
+  }
+
+  // Cuando el usuario da submit al formulario
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // Validar
+    if(ciudad.trim() === '' || pais.trim() === ''){
+      setError(true);
+      return;
+    }
+
+    setError(false);
+
+    // Pasarlo al componente principal
+  }  
+ 
   return ( 
-    <div>
-      Formulario
-    </div>
+    <form
+      onSubmit={handleSubmit}
+    >
+    {error ? <p className='red darken-4 error'>Todos los campos son obligatorios</p> : null }
+      <div className='input-field col s12'>
+        <input 
+          type='text'
+          name='ciudad'
+          id='ciudad'
+          value={ciudad}
+          onChange={handleChange}
+        /> 
+        <label htmlFor='ciudad'>Ciudad: </label>
+      </div>
+      <div className='input-field col s12'>
+        <select
+          name='pais'
+          id='pais'
+          value={pais}
+          onChange={handleChange}
+        >
+          <option value=''>--  Seleccione un país  --</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="PE">Perú</option>
+        </select>
+        <label htmlFor='pais'>País: </label>         
+        
+      </div>
+      <div className='input-field col s12'>
+        <input 
+          type='submit'
+          value='Buscar Clima'
+          className='waves-effect waves-light btn-large btn-block yellow accent-4 '
+        /> 
+      </div>
+    </form>
    );
 }
  
