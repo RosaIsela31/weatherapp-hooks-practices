8f84c61c78f131c036bd57ed2ee1ed43a69e30bb
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-30 11:43:56 -0600

    creatin fetch successful

--- src/component/AppContainer.js
+++ src/component/AppContainer.js
@@ -1,9 +1,39 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
 import '../index.css';
 
 const AppContainer = () => {
+
+   // State del componente principal
+   const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [consultar, guardarConsultar] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if(consultar){
+
+        const appId = 'd937ac867e384963e46a254c235b1ce7';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+  
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+  
+        console.log(resultado);
+      }
+      
+
+     
+    }
+    consultarAPI();
+  }, [consultar]);
+
   return ( 
     <Fragment>
       <Header 
@@ -13,7 +43,11 @@ const AppContainer = () => {
         <div className='container'>
           <div className='row'>
             <div className='col m6 s12'>
-              <Formulario />
+              <Formulario 
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
             </div>
             <div className='col m6 s12'>
               2
